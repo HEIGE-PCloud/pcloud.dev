6b649b2d3118e52c585349d939c1a185bd1ea296
HEIGE-PCloud <heige.pcloud@outlook.com>
2022-06-16 19:03:40 +0100

    feat: use css module to customize KaTeX

--- components/EquationBlock.tsx
+++ components/EquationBlock.tsx
@@ -1,4 +1,5 @@
 import katex from 'katex'
+import styles from './EquationBlock.module.css'
 import 'katex/dist/katex.css'
 import 'katex/contrib/mhchem'
 
@@ -12,7 +13,7 @@ export default function EquationBlock({ expression, displayMode, className }: Eq
     if (displayMode) {
         return (
             <div
-                className={className} 
+                className={styles.blockEquation} 
                 dangerouslySetInnerHTML={{__html: katex.renderToString(expression, {
                 throwOnError: false,
                 displayMode
