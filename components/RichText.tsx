931b168f1c4d45cd77aefd326e52ed42e194e313
HEIGE-PCloud <heige.pcloud@outlook.com>
2022-06-16 16:05:24 +0100

    fix: display empty line

--- components/RichText.tsx
+++ components/RichText.tsx
@@ -1,12 +1,35 @@
 import styles from "./RichText.module.css";
 import EquationBlock from './EquationBlock'
 
+interface Annotations {
+  bold: boolean,
+  italic: boolean,
+  strikethrough: boolean,
+  underline: boolean,
+  code: boolean,
+  color: 'default' | 'gray' | 'brown' | 'orange' | 'yellow' | 'green' | 'blue' | 'purple' | 'pink' | 'red' | 'gray_background' | 'brown_background' | 'orange_background' | 'yellow_background' | 'green_background' | 'blue_background' | 'purple_background' | 'pink_background' | 'red_background'
+}
+
+interface RichTextProps {
+  text: [
+    type: 'text',
+    text: {
+      content: string,
+      link?: string
+    },
+    annotations: Annotations,
+    plain_text: string,
+    href?: string
+  ] | [
+
+  ]
+}
+
 export function RichText({ text }) {
-  if (!text) {
-    return null;
+  if (text.length === 0) {
+    return <br/>
   }
   return text.map((value, index) => {
-    console.log(value)
     const { type, annotations: { bold, code, color, italic, strikethrough, underline } } = value
     const className = [
       bold ? styles.bold : "",
