d87e11566180798ba39f61b8e37617895a9789cf
HEIGE-PCloud <heige.pcloud@outlook.com>
2022-06-18 17:20:05 +0100

    refactor: use useEffect for code

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -1,31 +1,23 @@
 import { CodeBlock as CodeBlockType } from '../lib/notionTypes'
 import Prism from 'prismjs'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
 export default function CodeBlock({ block }: { block: CodeBlockType }) {
-  const [html, setHtml] = useState(null)
   const lang = block.code.language
   const code = block.code.rich_text.map((text) => text.plain_text).join('')
   useEffect(() => {
     import(`prismjs/components/prism-${lang}`)
       .then(() => {
-        setHtml(Prism.highlight(code, Prism.languages[lang], lang))
+        Prism.highlightAll()
       })
       .catch(() => {
-        setHtml(Prism.highlight(code, Prism.languages[lang], lang))
+        Prism.highlightAll()
       })
   })
 
   return (
-    <div>
-      <pre className={`language-${lang}`}>
-        <code
-          className={`language-${lang}`}
-          dangerouslySetInnerHTML={{
-            __html: html
-          }}
-        ></code>
-      </pre>
-    </div>
+    <pre className={`line-numbers language-${lang}`}>
+      <code className={`language-${lang}`}>{code}</code>
+    </pre>
   )
 }
