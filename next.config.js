8efc19b5bc4ea43d39217eeb3341e0e7ff2cfcc9
HEIGE-PCloud <heige.pcloud@outlook.com>
2022-06-19 17:50:33 +0100

    chore: add bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+module.exports = withBundleAnalyzer({
   images: {
     domains: ['images.unsplash.com', 's3.us-west-2.amazonaws.com'],
     minimumCacheTTL: 60
   }
-}
+})
